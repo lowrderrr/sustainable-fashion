070feb63384df68f40918ac9884a6d538ae98861
lowrderrr <165852321+lowrderrr@users.noreply.github.com>
2024-12-05 15:04:24 -0500

    Create main.js

---
+++ interactive-map/main.js
@@ -0,0 +1,10 @@
+var pieChartData = {
+    labels: ['United States', 'China', 'United Kingdom', 'Rep. of Korea', 'Germany', 'Pakistan', 'United Arab Emirates', 'Netherlands', 'Poland', 'Canada'],
+    datasets: [{
+        label: 'leading exporters of worn clothing worldwide',
+        data: [20, 58, 1, 5, 5,5,6],
+        backgroundColor: ['#36a2eb', '#ff6384', '#ffcd56', '#4caf50', '#f1c40f'],
+        borderColor: '#fff',
+        borderWidth: 1
+    }]
+};
